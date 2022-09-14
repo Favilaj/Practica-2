a2eeafbf277da56c24181748a7d73be21ba25707
WIN10PRO <WIN10PRO@DESKTOP-34KC35S>
2022-09-13 18:11:24 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad = 24;
+            string Nombre = "Jose Alejandro Favila Salazar";
+            string Pais = "Fracia";
+            Console.WriteLine(" Mi nombre es:Nombre {0}, tengo: Edad {1} de edad y me gustaria conocer el Pais {2}: ", Nombre, Edad, Pais);
+
+        }
+    }
+}
